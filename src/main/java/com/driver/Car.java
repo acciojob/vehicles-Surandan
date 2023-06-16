59585988fd6f136997e868ed6fdf5fbd8dfafe6f
Surandan <110124420+Surandan@users.noreply.github.com>
2023-06-16 19:21:39 +0530

    singsvn

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -65,9 +65,7 @@ public class Car extends Vehicle {
         this.seats = seats;
     }
 
-//    public Car() {
-//
-//    }
+
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -5,13 +5,11 @@ public class F1 extends Car {
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
         super(name,4,4,6,true,"sport",5);
-         this.setName(name);
-         this.setManual(isManual);
     }
 
     public void accelerate(int rate){
         int newSpeed = rate + getCurrentSpeed(); //set the value of new speed by using currentSpeed and rate
-//                                use this or noot
+
         /*
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -28,8 +26,7 @@ public class F1 extends Car {
             changeGear(1);
         }
         //for all other cases, change the gear accordingly
-
-        if(newSpeed >250){
+        else if(newSpeed >250){
             changeGear(6);
             changeSpeed(newSpeed,getCurrentDirection());
         }else if(newSpeed>200){
@@ -41,13 +38,10 @@ public class F1 extends Car {
         }else if(newSpeed>100){
             changeGear(3);
             changeSpeed(newSpeed,getCurrentDirection());
-        }
-        else if(newSpeed > 50){
+        } else if(newSpeed > 50){
             changeGear(2);
             changeSpeed(newSpeed,getCurrentDirection());
-        }
-//
-       else{
+        }else{
             changeGear(1);
             changeSpeed(newSpeed,getCurrentDirection());
         }
