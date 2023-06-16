d7ad9958d431b4369895043ad67f80af3d4ee207
Surandan <110124420+Surandan@users.noreply.github.com>
2023-06-16 18:34:02 +0530

    ehbf

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -10,7 +10,8 @@ public class F1 extends Car {
     }
 
     public void accelerate(int rate){
-        int newSpeed = rate + this.getCurrentSpeed(); //set the value of new speed by using currentSpeed and rate
+        int newSpeed = rate + getCurrentSpeed(); //set the value of new speed by using currentSpeed and rate
+//                                use this or noot
         /*
          * speed 0: gear 1
          * speed 1-50: gear 1
